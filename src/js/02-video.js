7bc38b7c68d63b2833d2e6ee2559c94055d219fa
Maxym <103531640+MfD0@users.noreply.github.com>
2023-11-02 19:43:41 +0200

    2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// Ініціалізація плеєра
+const vimeoPlayer = new Player('vimeo-player');
+
+// Відстеження події timeupdate
+vimeoPlayer.on('timeupdate', throttle(function (event) {
+  
+    // Збереження поточного часу відтворення у локальне сховище
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+}, 1000)); 
+
+// При завантаженні сторінки встановлення позиції відео
+window.addEventListener('load', function() {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+        vimeoPlayer.setCurrentTime(savedTime);
+    }
+});
 
