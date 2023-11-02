7592f0e398fbdd8115a888a0e24622a88578c124
Maxym <103531640+MfD0@users.noreply.github.com>
2023-11-02 19:38:43 +0200

    1 завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+// Функція для створення розмітки галереї
+function createGalleryMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  }).join('');
+}
+
+// Створення розмітки та ініціалізація галереї
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = galleryMarkup;
+
+// Ініціалізація SimpleLightbox з опціями
+new SimpleLightbox('.gallery a', { 
+  captionsData: 'alt', 
+  captionDelay: 250,
+  captionPosition: 'bottom'
+});
+
+
